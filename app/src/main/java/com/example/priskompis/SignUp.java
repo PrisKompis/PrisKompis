79bde2ef7a12284385df2b8cf07666508717bb65
SerenaB91 <serenabarakat2@gmail.com>
2019-04-11 14:39:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/priskompis/SignUp.java
@@ -0,0 +1,28 @@
+package com.example.priskompis;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SignUp extends AppCompatActivity {
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent1 = new Intent (SignUp.this,MainActivity.class);
+                startActivity(intent1);
+            }
+        });
+    }
+
+
+}
---
+++ app/src/main/java/com/example/priskompis/UsageStats.java
@@ -0,0 +1,13 @@
+package com.example.priskompis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UsageStats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usage_stats);
+    }
+}
