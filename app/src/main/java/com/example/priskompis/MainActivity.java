0e492793bcb60b9560ce24c0fa230c4573cd6ade
Dimitrios Tsamis <dhm.tsamhs@gmail.com>
2019-04-25 09:43:28 +0200

    Login and Signup Functions Added

--- app/src/main/java/com/example/priskompis/MainActivity.java
+++ app/src/main/java/com/example/priskompis/MainActivity.java
@@ -3,19 +3,40 @@ import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.firebase.client.Firebase;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
+
+private FirebaseAuth mAuth;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
 
-       int secondsDelayed = 1;
+
+    if (mAuth.getCurrentUser()!=null){
+    startActivity(new Intent(getApplicationContext(),BudgetSet.class));
+    }
+
+    else
+        {
+        startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+        }
+
+       /*int secondsDelayed = 1;
         new Handler().postDelayed(new Runnable() {
             public void run() {
                 startActivity(new Intent(MainActivity.this, LoginActivity.class));
                 finish();
             }
-        },   1000);
+        },   1000);*/
     }
 }
--- app/src/main/java/com/example/priskompis/SignUp.java
+++ app/src/main/java/com/example/priskompis/SignUp.java
@@ -1,25 +1,84 @@
 package com.example.priskompis;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
-    private Button button;
+private EditText email;
+private EditText pass;
+private Button btnSignup;
+private FirebaseAuth mAuth;
+private ProgressDialog mDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth=FirebaseAuth.getInstance();
 
-        button = findViewById(R.id.btn_reg);
-        button.setOnClickListener(new View.OnClickListener() {
+        mDialog=new ProgressDialog(this);
+
+        email=findViewById(R.id.btn_emailId);
+        pass=findViewById(R.id.btn_pwd);
+        btnSignup = findViewById(R.id.btn_reg);
+
+
+            btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent1 = new Intent (SignUp.this,LoginActivity.class);
-                startActivity(intent1);
+            public void onClick(View view) {
+
+            String mEmail=email.getText().toString().trim();
+            String mPass=pass.getText().toString().trim();
+
+            if (TextUtils.isEmpty(mEmail)){
+            email.setError("Required Field..");
+            return;
+            }
+            if (TextUtils.isEmpty(mPass)){
+            pass.setError("Required Field..");
+            return;
+            }
+
+            mDialog.setMessage("Processing..");
+            mDialog.show();
+
+            mAuth.createUserWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task)
+                    {
+                    if (task.isSuccessful())
+                        {
+                        mDialog.dismiss();
+                        startActivity(new Intent(getApplicationContext(), BudgetSet.class));
+                        Toast.makeText(getApplicationContext(), "Registration Complete", Toast.LENGTH_SHORT).show();
+
+                        }
+                    else
+                        {
+                        Toast.makeText(getApplicationContext(), "Problem", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+
+
+
             }
         });
     }
