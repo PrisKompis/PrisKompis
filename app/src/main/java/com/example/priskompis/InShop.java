8b8697856956b403e352f3acb6dff62659b5950f
Dimitrios Tsamis <dhm.tsamhs@gmail.com>
2019-04-26 00:34:24 +0200

    Added calculation for the total price of each product. Added demo method to display how the product details will show. Created onclick methods in order to clear EditTexts before entering new value

--- app/src/main/java/com/example/priskompis/InShop.java
+++ app/src/main/java/com/example/priskompis/InShop.java
@@ -3,7 +3,12 @@ package com.example.priskompis;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,6 +24,12 @@ public class InShop extends AppCompatActivity {
     private TextView displayName;
     private TextView displayQuantity;
     private TextView displayPrice;
+    private EditText requiredQuantity;
+    private TextView resultView;
+    private float reqQuantity=1;
+    private float result;
+    private TextView quantityLabel;
+    private Button addToCart;
 
 
 
@@ -30,6 +41,42 @@ public class InShop extends AppCompatActivity {
         displayName=findViewById(R.id.productNameDisplay);
     displayQuantity=findViewById(R.id.productQuantityDisplay);
     displayPrice=findViewById(R.id.productPriceDisplay);
+    requiredQuantity=findViewById(R.id.editQuantity);
+    resultView=findViewById(R.id.textViewResult);
+    quantityLabel=findViewById(R.id.quantityLabel);
+    addToCart=findViewById(R.id.buttonAddCart);
+
+    requiredQuantity.addTextChangedListener(new TextWatcher()
+        {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+
+            }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+
+            }
+
+        @Override
+        public void afterTextChanged(Editable s)
+            {
+            if (TextUtils.isEmpty(requiredQuantity.getText().toString())||requiredQuantity.getText().toString().endsWith("."))
+                {
+                resultView.setText("0");
+                }
+            else
+                {
+                reqQuantity = Float.parseFloat(requiredQuantity.getText().toString());
+                float price = product.getPriceICA();
+                result = price * reqQuantity;
+                result=(float)Math.ceil(result);
+                resultView.setText(String.valueOf(result));
+                }
+            }
+        });
         budget=intent.getIntExtra("budget",0);
 
 
@@ -43,6 +90,34 @@ public void scanItem(View view)
     displayName.setText(product.getName());
     displayQuantity.setText(product.getQuantity());
     displayPrice.setText(String.valueOf(product.getPriceICA())+" SEK");
+    resultView.setText(String.valueOf(product.getPriceICA()));
+    requiredQuantity.setText("1");
+    addToCart.setVisibility(View.VISIBLE);
+    resultView.setVisibility(View.VISIBLE);
+    quantityLabel.setVisibility(View.VISIBLE);
+    requiredQuantity.setVisibility(View.VISIBLE);
 
+
+    }
+
+
+public void AddToCart(View view)
+    {
+    if(TextUtils.isEmpty(requiredQuantity.getText()))
+        {
+        requiredQuantity.setError("Quantity can not be empty");
+
+        return;
+        }
+    reqQuantity=Float.parseFloat(requiredQuantity.getText().toString());
+    float price=product.getPriceICA();
+    result=price*reqQuantity;
+    result=(float)Math.ceil(result);
+
+    }
+
+public void clearText(View view)
+    {
+        requiredQuantity.getText().clear();
     }
 }
