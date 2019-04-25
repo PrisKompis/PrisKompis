1c838df6529328e719fbf93eb0308561f3cdc161
Harshi Wanasinghe <wanasinghe.harshi@gmail.com>
2019-04-26 00:50:19 +0200

    set budget to progress chart

--- app/src/main/java/com/example/priskompis/InShop.java
+++ app/src/main/java/com/example/priskompis/InShop.java
@@ -4,15 +4,16 @@ import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class InShop extends AppCompatActivity {
 
     Intent intent =new Intent();
-    private int userBudget;
+    private int userSetBudget;
     private int currentItemPrice = 0;
-    private int budget = 0;
+    //private int userSetBudget = 0;
     private int totalPrice =0;
     AlertDialog.Builder builder;
 
@@ -25,9 +26,23 @@ public class InShop extends AppCompatActivity {
         intent=getIntent();
         setContentView(R.layout.activity_in_shop);
 
-        userBudget=intent.getIntExtra("budget",0);
-
-
+        userSetBudget=intent.getIntExtra("budget",0);
+        updateChart();
 
     }
+
+    private void updateChart(){
+        // Update the text in a center of the chart:
+        TextView totalBudget = findViewById(R.id.total_budget);
+        totalBudget.setText(String.valueOf(totalPrice) + " / " + userSetBudget);
+
+        // Calculate the slice size and update the pie chart:
+        ProgressBar pieChart = findViewById(R.id.stats_progressbar);
+        double d = (double) totalPrice / (double) userSetBudget;
+        int progress = (int) (d * 100);
+        pieChart.setProgress(progress);
+    }
+
+
+
 }
