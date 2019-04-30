d000a5461df0d77afc599a0de18806845fd81c9e
Saranya Seetharaman <saranyas72@gmail.com>
2019-04-30 11:52:23 +0200

    Commented test data used for barcode and uncommented actual scanner code

--- app/src/main/java/com/example/priskompis/InShop.java
+++ app/src/main/java/com/example/priskompis/InShop.java
@@ -116,8 +116,8 @@ public class InShop extends AppCompatActivity
                     barcode = data.getParcelableExtra(BarcodeCaptureActivity.BarcodeObject);
                     //statusMessage.setText(R.string.barcode_success);
                     barcodeValue.setText(barcode.displayValue);
-                    //updateProduct(barcode.rawValue.substring(0, barcode.rawValue.length() - 1));
-                    updateProduct("0000042");
+                    updateProduct(barcode.rawValue.substring(0, barcode.rawValue.length() - 1));
+                    //updateProduct("0000042");
                     Log.d(TAG, "Barcode read: " + barcode.displayValue);
 
                 } else {
