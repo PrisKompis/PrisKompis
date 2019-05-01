cff8f7e45f95c4b1e9d46168e2f9d328672b902a
SerenaB91 <serenabarakat2@gmail.com>
2019-05-01 20:34:53 +0200

    added design for login and signup activities

--- app/src/main/java/com/example/priskompis/LoginActivity.java
+++ app/src/main/java/com/example/priskompis/LoginActivity.java
@@ -2,15 +2,16 @@ package com.example.priskompis;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -25,17 +26,34 @@ public class LoginActivity extends AppCompatActivity {
     private ProgressDialog mDialog;
     private EditText email;
     private EditText password;
+    private ImageView logo;
+    Animation frombottom;
+    Animation fromtop;
+    Animation fadein;
+    Animation forlogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
         setContentView(R.layout.activity_login);
+
         mAuth=FirebaseAuth.getInstance();
+
+        frombottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
+        fromtop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
+        forlogo = AnimationUtils.loadAnimation(this,R.anim.forlogo);
 
         mDialog=new ProgressDialog(this);
 
         email=findViewById(R.id.email_reg);
+        email.setAnimation(fromtop);
         password=findViewById(R.id.password_reg);
+        password.setAnimation(fromtop);
 
         btnSignIn=findViewById(R.id.btnsignin_reg);
         btnSignup = findViewById(R.id.btnsignup_reg);
@@ -47,13 +65,19 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        logo=findViewById(R.id.imageView3);
+        logo.setAnimation(forlogo);
+        btnSignIn.setAnimation(fromtop);
+        btnSignup.setAnimation(frombottom);
     }
+
 
     public void loginToApp(View view) {
 
         {
-            final String mEmail=email.getText().toString().trim();
-            final String mPass=password.getText().toString().trim();
+
+            String mEmail=email.getText().toString().trim();
+            String mPass=password.getText().toString().trim();
 
             if (TextUtils.isEmpty(mEmail)){
                 email.setError("Required Field..");
@@ -75,8 +99,8 @@ public class LoginActivity extends AppCompatActivity {
                     if (task.isSuccessful())
                     {
                         mDialog.dismiss();
-                        setEmailToPreferences(mEmail);
                         startActivity(new Intent(getApplicationContext(), BudgetSet.class));
+
                         Toast.makeText(getApplicationContext(), "Login Complete", Toast.LENGTH_SHORT).show();
                     }
                     else
@@ -89,15 +113,6 @@ public class LoginActivity extends AppCompatActivity {
 
             });
 
-
         }
-    }
-
-    public void setEmailToPreferences(String email) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor edit = preferences.edit();
-        edit.putString("userEmail", email);
-        edit.apply();
-
     }
 }
--- app/src/main/java/com/example/priskompis/SignUp.java
+++ app/src/main/java/com/example/priskompis/SignUp.java
@@ -7,8 +7,11 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -17,78 +20,101 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class SignUp extends AppCompatActivity {
-private EditText email;
-private EditText pass,repass;
-private Button btnSignup;
-private FirebaseAuth mAuth;
-private ProgressDialog mDialog;
+    private EditText email;
+    private EditText pass,repass;
+    private Button btnSignup;
+    private FirebaseAuth mAuth;
+    private ProgressDialog mDialog;
+    private ImageView logo;
+    Animation frombottom;
+    Animation fromtop;
+    Animation forlogo;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        try
+        {
+            this.getSupportActionBar().hide();
+        }
+        catch (NullPointerException e){}
         setContentView(R.layout.activity_sign_up);
         mAuth=FirebaseAuth.getInstance();
 
+        frombottom = AnimationUtils.loadAnimation(this,R.anim.frombottom);
+        fromtop = AnimationUtils.loadAnimation(this,R.anim.fromtop);
+        forlogo = AnimationUtils.loadAnimation(this,R.anim.forlogo);
+
         mDialog=new ProgressDialog(this);
 
+        logo=findViewById(R.id.imageView);
+        logo.setAnimation(forlogo);
+
         email=findViewById(R.id.btn_emailId);
+        email.setAnimation(fromtop);
+
         pass=findViewById(R.id.btn_pwd);
+        pass.setAnimation(fromtop);
+
         repass=findViewById(R.id.btn_retypepwd);
+        repass.setAnimation(fromtop);
+
         btnSignup = findViewById(R.id.btn_reg);
+        btnSignup.setAnimation(frombottom);
 
 
-            btnSignup.setOnClickListener(new View.OnClickListener() {
+        btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view)
-                {
+            {
 
                 String mEmail = email.getText().toString().trim();
                 String mPass = pass.getText().toString().trim();
                 String mRepass = repass.getText().toString().trim();
 
                 if (TextUtils.isEmpty(mEmail))
-                    {
+                {
                     email.setError("Required Field..");
                     return;
-                    }
+                }
                 if (TextUtils.isEmpty(mPass))
-                    {
+                {
                     pass.setError("Required Field..");
                     return;
-                    }
+                }
 
                 if (TextUtils.isEmpty(mRepass))
-                    {
+                {
                     repass.setError("Required Field");
                     return;
-                    }
+                }
 
                 if (!(mPass.equals(mRepass)))
-                    {Toast.makeText(getApplicationContext(), "Passwords do not match!", Toast.LENGTH_SHORT);
+                {Toast.makeText(getApplicationContext(), "Passwords do not match!", Toast.LENGTH_SHORT);
 
                 }
 
 
 
-            mDialog.setMessage("Processing..");
-            mDialog.show();
+                mDialog.setMessage("Processing..");
+                mDialog.show();
 
-            mAuth.createUserWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>()
+                mAuth.createUserWithEmailAndPassword(mEmail,mPass).addOnCompleteListener(new OnCompleteListener<AuthResult>()
                 {
-                @Override
-                public void onComplete(@NonNull Task<AuthResult> task)
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task)
                     {
-                    if (task.isSuccessful())
+                        if (task.isSuccessful())
                         {
-                        mDialog.dismiss();
-                        startActivity(new Intent(getApplicationContext(), BudgetSet.class));
-                        Toast.makeText(getApplicationContext(), "Registration Complete", Toast.LENGTH_SHORT).show();
+                            mDialog.dismiss();
+                            startActivity(new Intent(getApplicationContext(), BudgetSet.class));
+                            Toast.makeText(getApplicationContext(), "Registration Complete", Toast.LENGTH_SHORT).show();
 
                         }
-                    else
+                        else
                         {
-                        Toast.makeText(getApplicationContext(), "Problem", Toast.LENGTH_SHORT).show();
+                            Toast.makeText(getApplicationContext(), "Problem", Toast.LENGTH_SHORT).show();
 
                         }
                     }
